8aad777ce9996b0f4b3ae7677f20e98bc587ceab
HeidiBrooke <hbrooke36@yahoo.com>
2023-01-31 18:48:34 -0800

    Add form input for data fetching

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 async function getWeather() {
+    console.log(`I'm running!`)
     try{
-        //fetch data for Portland
-    const theURL = 'https://api.openweathermap.org/data/2.5/weather?q=Portland,us&APPID=db3042e2b3f071ad96934f9cfe021850';
+    //fetch data 
+    const city = document.getElementById('city').value;
+    const state = document.getElementById('state').value;
+    const country = document.getElementById('country').value;
+    const theURL = `https://api.openweathermap.org/data/2.5/weather?q=${city},${state},${country}&APPID=db3042e2b3f071ad96934f9cfe021850`;
     const data = await fetch(theURL, {mode: 'cors'});
     console.log(data);
     //interpret data
@@ -10,11 +14,13 @@ async function getWeather() {
     console.log(processedData); 
     const feelsLike = processedData.main.feels_like;
     const wind = processedData.wind.speed;
+    const display = document.getElementById('display');
+    display.textContent = `The temperature is ${toFarenheit(feelsLike)} in ${processedData.name}`;
     console.log(wind);
     console.log(toCelsius(feelsLike));
     console.log(toFarenheit(feelsLike));
     }catch{
-        console.log('sorry, Im broke');
+        display.textContent = `The temperature in ${processedData.name} Please try again.`;
     }
 }
 
@@ -26,4 +32,5 @@ function toCelsius(K) {
     return (K-273.15).toFixed(2);
 }
 
-getWeather();
+const button = document.getElementById('button');
+button.addEventListener('click', getWeather);
